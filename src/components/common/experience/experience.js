82276b5f4c97f10564f6e0f7589cbd14de8d177e
unknown <jaywapp16@gmail.com>
2023-03-20 19:51:19 +0900

    update

--- src/components/common/experience/experience.js
+++ src/components/common/experience/experience.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Expes } from "../../../data/information";
-import { Project } from "../project/project";
 import { Header, SeperatableHeader, Seperator } from "../header";
 
 export function Experiences() {
@@ -30,7 +29,8 @@ function Experience(experience) {
             <Roles roles={experience.roles} />
             <Date enter={experience.enterDate} resign={experience.resignDate} />
             <Works works={experience.works} />
-            <Performances performances={experience.performances} />
+            <Projects projects={experience.projects}/>
+            {/* <Performances performances={experience.performances} /> */}
         </Tag>
     )
 }
@@ -113,6 +113,124 @@ function Work({ work }) {
     )
 }
 
+function Projects({projects}){
+    let Tag = styled.div`
+        margin-top: 5px;
+        margin-bottom: 5px;
+    `;
+
+    return (
+        <Tag>
+            <Header level={3} title={"프로젝트"} />
+            {
+                projects.map(p => {
+                    return (
+                        <Project project={p} />
+                    )
+                })
+            }
+
+        </Tag>
+    )
+
+}
+
+function Project({project}){
+
+    let Tag = styled.div`
+        margin-top: 5px;
+        margin-bottom: 5px;
+        
+        display: grid;
+        grid-template-columns: auto 1fr;
+        border: 1px solid black;
+    `;
+
+    let LeftTag = styled.div`
+        grid-row: ${(props) => props.row};
+        grid-column: 1;
+        width: 100px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border-right: 1px solid;
+        font-weight: bold;
+        text-align: center;
+    `;
+
+    let RightTag = styled.div`
+        grid-row: ${(props) => props.row};
+        grid-column: 2;
+
+        margin-left: 10px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    `;
+
+    return (
+        <Tag>
+            <LeftTag row={1}>이름</LeftTag>
+            <LeftTag row={2}>설명</LeftTag>
+            <LeftTag row={3}>기간</LeftTag>
+            <LeftTag row={4}>역할</LeftTag>
+            <LeftTag row={5}>성과</LeftTag>
+            <LeftTag row={6}>기술</LeftTag>
+
+            <RightTag row={1}>{project.name}</RightTag>
+            <RightTag row={2}>{project.desc}</RightTag>
+            <RightTag row={3}>{project.date}</RightTag>
+            <RightTag row={4}>
+                <List items={project.actions}/>
+            </RightTag>
+            <RightTag row={5}>
+                <List items={project.results}/>
+            </RightTag>
+            <RightTag row={6}>
+                {Join(project.skills, ',')}
+            </RightTag>
+
+        </Tag>
+    )
+}
+
+function List({items}){
+    let Tag = styled.div`
+    `;
+
+    let Unit = styled.div`
+        margin-top: 2px;
+        margin-bottom: 2px;
+    `; 
+
+    let index = 1;
+
+    return (
+        <Tag>
+            {
+                items.map(item => {
+                    return(
+                        <Unit>{index++ + '. ' + item}</Unit>
+                    )
+                })
+            }
+        </Tag>
+    )
+}
+
+function Join(strs, seperator){
+    let result = '';
+    let index = 1;
+
+    strs.forEach(str => {
+        result += str;
+        if(strs.length != index++){
+            result += seperator;
+            result += ' ';
+        }
+    });
+
+    return result;
+}
+
 function Performances({ performances }) {
     let Tag = styled.div`
         margin-top: 5px;
@@ -145,7 +263,7 @@ function Performance({ performance }) {
         <Tag>
             <Header level={4} title={performance.name} />
             <PerformanceDesc desc={performance.desc} />
-            <PerformanceSummary summary={performance.summary} />
+            <PerformanceActions actions={performance.actions} />
         </Tag>
     )
 }
@@ -164,7 +282,7 @@ function PerformanceDesc({ desc }) {
     )
 }
 
-function PerformanceSummary({ summary }) {
+function PerformanceActions({ actions }) {
     let Tag = styled.div`
         margin-top: 5px;
         margin-bottom: 5px;
@@ -176,9 +294,9 @@ function PerformanceSummary({ summary }) {
     return (
         <Tag>
             {
-                summary.map(s => {
+                actions.map(s => {
                     return (
-                        <PerformanceSummaryUnit idx={idx++} unit={s} />
+                        <PerformanceAction idx={idx++} action={s} />
                     )
                 })
             }
@@ -186,14 +304,14 @@ function PerformanceSummary({ summary }) {
     )
 }
 
-function PerformanceSummaryUnit({ idx, unit }) {
+function PerformanceAction({ idx, action }) {
     let Tag = styled.div`
         margin-top: 5px;
         margin-bottom: 5px;
     `;
 
     return (
-        <Tag>{idx + '. ' + unit}</Tag>
+        <Tag>{idx + '. ' + action}</Tag>
     )
 }
 
